b7239ac9caad136e5b5ff16ce7b9f779351fd116
Alif Al Razi <103985669+ALIF-AL-RAZI@users.noreply.github.com>
2024-11-27 21:28:08 +0600

    Update index.js

--- frontend/src/api/axios/index.js
+++ frontend/src/api/axios/index.js
@@ -8,7 +8,7 @@ export default axios.create({
 });
 
 export const axiosPrivate = axios.create({
-    baseURL,
+    baseURL: 'https://tweeter-hootspy.onrender.com/api/v1',
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 });

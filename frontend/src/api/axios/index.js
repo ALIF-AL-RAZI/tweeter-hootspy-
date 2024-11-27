b9fc945f0414c42a237dafbc40e942a869163cc4
Alif Al Razi <103985669+ALIF-AL-RAZI@users.noreply.github.com>
2024-11-27 21:31:39 +0600

    Update index.js

--- frontend/src/api/axios/index.js
+++ frontend/src/api/axios/index.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-// export const baseURL =
-//     process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api/v1' : 'https://tweeter-hootspy.onrender.com/api/v1';
+export const baseURL =
+    process.env.NODE_ENV === 'https://tweeter-hootspy.onrender.com/api/v1';
 
 export default axios.create({
-    baseURL : 'https://tweeter-hootspy.onrender.com/api/v1',
+    baseURL,
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: 'https://tweeter-hootspy.onrender.com/api/v1',
+    baseURL,
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 });
